a22a3bfd1b97f75b63156f2dcc90ea7c9b2cfeaa
Ruhi2023 <132193880+Ruhi2023@users.noreply.github.com>
2023-07-06 17:15:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 #reading file
-with open('input.txt','r',encoding='utf-8') as f:
-  text = f.read()
-print("length of dataset in characters:", len(text))
+with open('Data-input.txt', 'r', encoding='utf-8') as f:
+  tx= f.read()
+print("length of dataset in characters:",len(tx))
+chars = sorted(list(set(tx)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
+sti = {ch:i for i,ch in enumerate(chars)}
+its = {i:ch for i,ch in enumerate(chars)}
+encode = lambda s: [sti[c] for c in s]
+decode = lambda l: ''.join([its[i] for i in l])
+print(encode("hello world"))
+print(decode(encode("hello world")))
+import torch
+data = torch.tensor(encode(tx), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000])
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
